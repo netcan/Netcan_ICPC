c590439c3ad0a6244aa69d6683933083bf763a31
netcan <1469709759@qq.com>
2015-07-26 22:24:56 +0800

    水题

---
+++ XCOJ/1033.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int x,y,num=0;
+	char z[5];
+	char opt;
+	while(scanf("%d%c%d=%s",&x,&opt,&y,z)!=EOF)
+	{
+		if(z[0]=='?')
+			continue;
+		if(opt == '+')
+			if(x+y == atof(z)) num++;
+		if( opt == '-' )
+			if(x-y ==atof(z)) num++;
+		}
+	printf("%d\n",num);
+	return 0;
+}
