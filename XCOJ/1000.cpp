a45d48437a86e0732bec9a1d77e6f5e52a63b29b
netcan <1469709759@qq.com>
2015-07-26 22:23:58 +0800

    水题

---
+++ XCOJ/1000.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b;
+
+    while(scanf("%d %d",&a, &b) != EOF)
+
+        printf("%d\n",a+b);
+
+    return 0;
+
+}
+/**************************************************************
+    Problem: 1000
+    User: 2014218760
+    Language: C
+    Result: 正确
+    Time:0 ms
+    Memory:948 kb
+****************************************************************/
