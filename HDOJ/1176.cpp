0e126951d008e55083597251c5b3f4d75354c074
netcan <1469709759@qq.com>
2015-08-07 19:37:33 +0800

    动态规划，字符串， 最小操作数

--- HDOJ/1176.cpp
+++ HDOJ/1176.cpp
@@ -1,9 +1,9 @@
 /*************************************************************************
-	> File Name: 1176.cpp
+	> File Name: 最大子序列和O(N).cpp
 	  > Author: Netcan
 	  > Blog: http://www.netcan.xyz
 	  > Mail: 1469709759@qq.com
-	  > Created Time: 2015-07-30 Thu 15:45:51 CST
+	  > Created Time: Sat 06 Jun 2015 01:20:10 PM CST
  ************************************************************************/
 
 #include <iostream>
@@ -27,39 +27,38 @@
 #include <utility>
 #include <cstring>
 using namespace std;
-int n;
-int dp[100005][11];
-
-
-void solve() {
-	int n;
-	while(scanf("%d", &n) == 1 && n) {
-		memset(dp, 0, sizeof(dp));
-		int x, t, maxt = 0;
-		for (int i = 1; i <= n; ++i) {
-			scanf("%d%d", &x, &t);
-			++dp[t][x];
-			maxt = max(maxt, t);
-		}
-		for(int t=maxt-1; t>=0; --t) // 时间t
-			for(int x=0; x<=10; ++x) // 位置x
-				if(x==0)
-					dp[t][x] += max(dp[t+1][1], dp[t+1][0]);
-				else if(x==10)
-					dp[t][x] += max(dp[t+1][10], dp[t+1][9]);
-				else
-					dp[t][x] += max(max(dp[t+1][x], dp[t+1][x-1]), dp[t+1][x+1]);
-		cout << dp[0][5] << endl;
-
-	}
-
-}
 
 int main()
 {
 #ifdef Oj
-	freopen("1176.in", "r", stdin);
 #endif
-	solve();
+	int n;
+	cin >> n;
+	int data[10006];
+		for (int i = 0; i < n; ++i)
+			cin >> data[i];
+		int maxsum = -1;
+		int sum = 0;
+		int s, e;
+		int istart=0;
+		s = e = 0;
+		for (int i = 0; i < n; ++i) {
+			sum += data[i];
+			if(sum > maxsum) {
+				e = i;
+				maxsum = sum;
+				s = istart;
+			}
+			if(sum <0) {
+				sum = 0;
+				istart = i+1;
+			}
+		}
+
+		if(maxsum == -1)
+			printf("0 %d %d\n",0, n-1);
+		else
+		printf("%d %d %d\n", maxsum, s,e);
+
 	return 0;
 }
--- HDOJ/1536.cpp
+++ HDOJ/1536.cpp
@@ -28,18 +28,18 @@
 #include <cstring>
 using namespace std;
 
-int S[101], g[10005]; // 题目中的步数集合S, 以及sg(t)函数值g(t)
+int S[101], sg[10005]; // 题目中的步数集合S, 以及sg(t)函数值sg(t)
 int k, m, l, h, s_nim; // 集合大小k, 计算次数m, 堆数l, 每堆个数h, 异或和s_nim
 
-int met(int p) { // 求sg(t)值函数met(t)
+int SG(int p) { // 求sg(t)值函数met(t)
 	bool mark[101] = {false}; // 标记各个sg(t)的值，为了方便求补集最小值(sg(t))
 	for(int i=0; i<k; ++i) {
 		int t = p - S[i];
 		if(t < 0) // 小于0则退出循环，求出该层的sg(t)值
 			break;
-		if(g[t] == -1)
-			g[t] = met(t); // 递归求sg(t)
-		mark[g[t]] = true; // 标记该层的sg(t)值
+		if(sg[t] == -1)
+			sg[t] = SG(t); // 递归求sg(t)
+		mark[sg[t]] = true; // 标记该层的sg(t)值
 	}
 	for(int i=0;; ++i) // 求出该层的sg(t)值，即补集的最小值
 		if(!mark[i])
@@ -51,15 +51,15 @@ void solve() {
 		for(int i=0; i<k; ++i)
 			cin >> S[i];
 		sort(S, S+k); // 记得排序
-		memset(g, -1, sizeof(g));
+		memset(sg, -1, sizeof(sg));
 		cin >> m;
 		while(m--) {
 			s_nim = 0;
 			cin >> l;
 			while(l--) {
 				cin >> h;
-				g[h] = met(h);
-				s_nim^=g[h]; // 求异或和s_nim
+				sg[h] = SG(h);
+				s_nim^=sg[h]; // 求异或和s_nim
 			}
 			if(s_nim == 0)
 				cout << 'L';
