f5b61210b6ab68e651a47534c8d7c0ae8865951a
netcan <1469709759@qq.com>
2015-12-29 16:32:48 +0800

    尺取法

--- POJ/3320.cpp
+++ POJ/3320.cpp
@@ -21,17 +21,15 @@ void solve() {
 	int ans = P;
 	int lstsum = 0, crtsum = 0;
 	while(t < P) {
-		if(cnt[id[t++]]++ == 0) ++crtsum;
+		if(cnt[id[t++]]++ == 0) ++crtsum; // 遇见新知识点
 
-		if(crtsum > lstsum) {
+		if(crtsum > lstsum) { // 那么就要更新ans了。
 			ans = t - s;
 			lstsum  = crtsum;
 			// printf("%d\n", ans);
 		}
 
-		while(s < t && cnt[id[s]] > 1) {
-			cnt[id[s++]]--;
-		}
+		while(s < t && cnt[id[s]] > 1) cnt[id[s++]]--; // 检查前面是否有重复出现过的知识点，最后更新最小ans
 		ans = min(ans, t-s);
 
 	}
