2973026202cb82e0329e555c2fb1670c16f67fee
netcan <1469709759@qq.com>
2016-01-19 11:53:05 +0800

    增加线段树部分。

---
+++ My_Template/push.sh
@@ -0,0 +1,21 @@
+#########################################################################
+# File Name: push.sh
+# Author: Netcan
+# Blog: http://www.netcan666.com
+# mail: 1469709759@qq.com
+# Created Time: 2016-01-19 二 11:47:16 CST
+#########################################################################
+#!/bin/bash
+
+git add acm_icpc_logo.png
+git add acm_template_by_netcan.aux
+git add acm_template_by_netcan.idx
+git add acm_template_by_netcan.out
+git add acm_template_by_netcan.pdf
+git add acm_template_by_netcan.tex
+git add acm_template_by_netcan.toc
+git add hfut_logo.jpg
+git add sodc.png
+git add push.sh
+git commit -m "增加线段树部分。"
+git push -u origin master
